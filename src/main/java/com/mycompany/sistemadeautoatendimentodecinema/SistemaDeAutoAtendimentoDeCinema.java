971ce397d7d833ae27f589c34896a729c0476ba6
Luis Guilherme <lgdilllg@gmail.com>
2025-10-17 11:50:45 -0400

    criando tela de login
    criando tela de login

---
+++ src/main/java/com/mycompany/sistemadeautoatendimentodecinema/SistemaDeAutoAtendimentoDeCinema.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemadeautoatendimentodecinema;
+
+/**
+ *
+ * @author Aluno
+ */
+public class SistemaDeAutoAtendimentoDeCinema {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
